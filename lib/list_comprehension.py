bc0a2c352b8b8d60a2b4892f913953f9e618cf02
Leroy G <liguan698@gmail.com>
2023-07-08 15:59:51 -0700

    complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    evens = [n for n in num_list if n%2 == 0]
+    return evens
     pass
 
 def make_exclamation(sentence_list):
-    pass
+    sentence_exclamation= [sentence +'!' for sentence in sentence_list if sentence is not None]
+    return sentence_exclamation
